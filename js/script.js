da21d338e7f778c7c8395be3a40b3acd3c61dfac
mohammad jalifathi <m.h.jalifathi@gmail.com>
2023-10-23 18:03:01 +0330

    faq section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// faq accordion
+document.addEventListener('DOMContentLoaded', () => {
+  const faqContainer = document.querySelector('.faq-content');
+  faqContainer.addEventListener('click', (e) => {
+    const groupHeader = e.target.closest('.faq-group-header');
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector('.faq-group-body');
+    const icon = groupHeader.querySelector('i');
+  });
+});
